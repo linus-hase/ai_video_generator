78767ddab237edcf6ab08a9f678263c486722203
Linus Hase <linushase.prvt@gmail.com>
2024-01-13 00:53:51 +0100

    fix apis and now working video generation

--- logic/generator.js
+++ logic/generator.js
@@ -1,42 +1,51 @@
 'use strict';
 
-require('dotenv').config();
 const fs = require('fs');
-const openai = require('openai-api');
-const { TextToSpeechClient } = require('@google-cloud/text-to-speech');
+const util = require('util');
+const OpenAI = require("openai");
+const textToSpeech = require('@google-cloud/text-to-speech');
+require('dotenv').config();
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
 const readline = require('readline');
+const mm = require('music-metadata');
+
+let videoTitle = "Lorem Ipsum";
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-const OPENAI_API_KEY = process.env.OPENAI_API;
-const openaiClient = new openai(OPENAI_API_KEY);
-const textToSpeechClient = new TextToSpeechClient();
+// api clients
+const OPENAI_API_KEY = process.env['OPENAI_API'];
+const openai = new OpenAI({
+    apiKey: OPENAI_API_KEY
+});
+const textToSpeechClient = new textToSpeech.TextToSpeechClient();
 
 async function getVideoContent() {
     return new Promise((resolve) => {
         rl.question("\nEnter the name of the video > ", (title) => {
+            videoTitle = title;
             rl.question('Do you want AI to generate content? (yes/no) > ', (option) => {
                 if (option === 'yes') {
                     rl.question("\nEnter the theme of the video > ", async (theme) => {
-                        const response = await openaiClient.chat.completions.create({
-                            engine: 'davinci',
-                            prompt: `Generate content on - "${theme}"`,
-                            temperature: 0.7,
-                            max_tokens: 200,
-                            top_p: 1,
-                            frequency_penalty: 0,
-                            presence_penalty: 0
-                        });
+                        const response = await openai.chat.completions.create({
+                            model:"gpt-3.5-turbo-1106",
+                            response_format:{ "type": "json_object" },
+                            messages:[
+                                {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+                                {"role": "user", "content": theme}
+                            ],
+                    });
 
-                        console.log(response.data.choices[0].text);
+                    console.log(response.choices[0].message.content);
 
                         rl.question('\nIs this fine? (yes/no) > ', (yesNo) => {
                             if (yesNo === 'yes') {
-                                resolve(response.data.choices[0].text);
+                                resolve(response.choices[0].message.content);
                             } else {
                                 rl.question('\nEnter > ', (content) => {
                                     resolve(content);
@@ -74,15 +83,22 @@ async function generateVideo() {
     // Randomly select gameplay footage
     const gp = Math.floor(Math.random() * 2) + 1;
     const start_point = Math.floor(Math.random() * 480);
-    const audio_clip = 'generated/speech.mp3';
+    const audio_clip = './generated/speech.mp3';
+    const audio_length = await mm.parseFile(audio_clip);
 
     // Video editing using ffmpeg-fluent
-    ffmpeg(`gameplay/gameplay_${gp}.mp4`)
-        .input(audio_clip)
+    ffmpeg(`./logic/gameplay/gameplay_1.mp4`)
         .setStartTime(start_point)
-        .duration(58) // set duration to 58 seconds or adjust as needed
-        .outputOptions('-aspect 9:16') // Set aspect ratio to 9:16
-        .save(`generated/${title}.mp4`)
+        .duration(audio_length.format.duration)
+        .addInput(audio_clip)
+        .complexFilter([
+            {
+            filter : 'amix', options: { inputs : 2, duration : 'longest' }
+            }
+        ])
+        .audioCodec('aac')
+        .videoCodec('libx264')
+        .save(`generated/${videoTitle}.mp4`)
         .on('end', () => {
             console.log('Video has been created.');
         });
